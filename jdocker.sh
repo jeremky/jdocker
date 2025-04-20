a9f7305f217fc0ab0e4cc10222c483b8ef697c84
JeremKy <jeremky@me.com>
2025-04-20 02:05:07 +0200

    Mise à jour

--- jdocker.sh
+++ jdocker.sh
@@ -16,8 +16,10 @@ fi
 if [[ ! -f /usr/bin/$dockerapp ]] && [[ -f /usr/bin/apt ]]; then
   echo "Installation de Podman..."
   sudo apt install -y podman podman-compose
+  if [[ -d $containersdir ]]; then
   sudo mkdir -p $containersdir
   sudo chown $user: $containersdir
+  fi
   if [[ $rootless = "on" ]]; then
     systemctl enable --user podman-restart.service
     systemctl enable --user podman.socket
@@ -48,158 +50,158 @@ fi
 
 # Commandes
 case $1 in
-list | ls)
-  $sudo $dockerapp container ls -a --format "table {{.Names}} \t {{.Status}}"
-  ;;
-listall | lsa)
-  $sudo $dockerapp container ls -a --format "table {{.Names}} \t {{.Status}} \t {{.Ports}} \t {{.Image}}"
-  ;;
-install | it)
-  shift
-  for app in $*; do
-    if [[ ! -f $dir/cfg/$app/compose.yml ]] || [[ -z "$1" ]]; then
-      echo "Application $app non trouvée"
-      echo ""
-    else
-      $sudo $compose -f $dir/cfg/$app/compose.yml up -d
-    fi
-  done
-  ;;
-remove | rm)
-  shift
-  for app in $*; do
-    if [[ ! -f $dir/cfg/$app/compose.yml ]] || [[ -z "$1" ]]; then
-      echo "Application $app non trouvée"
-      echo ""
-    else
-      $sudo $compose -f $dir/cfg/$app/compose.yml down
-    fi
-  done
-  ;;
-restart | r)
-  if [[ ! -z "$2" ]]; then
+  list | ls)
+    $sudo $dockerapp container ls -a --format "table {{.Names}} \t {{.Status}}"
+    ;;
+  listall | lsa)
+    $sudo $dockerapp container ls -a --format "table {{.Names}} \t {{.Status}} \t {{.Ports}} \t {{.Image}}"
+    ;;
+  install | it)
     shift
     for app in $*; do
-      $sudo $dockerapp restart $app
-    done
-  fi
-  ;;
-purge | pr)
-  $sudo $dockerapp system prune -f
-  ;;
-purgeall | pra)
-  $sudo $dockerapp system prune -f -a --volumes
-  ;;
-load | lo)
-  if [[ ! -d $imgdir/.old ]]; then
-    mkdir -p $imgdir/.old
-  fi
-  for file in $(ls $imgdir/*.tar); do
-    $sudo $dockerapp load -i $file
-    mv $file $imgdir/.old
-  done
-  ;;
-upgrade | up)
-  if [[ ! -z "$2" ]]; then
-    shift
-    for app in $*; do
-      $dir/jdocker.sh rm $app
-      $dir/jdocker.sh it $app
-    done
-  else
-    $sudo $dockerapp images | grep -v ^REPO | sed 's/ \+/:/g' | cut -d: -f1,2 | xargs -L1 $sudo $dockerapp pull
-  fi
-  ;;
-logs | l)
-  if [[ -z "$3" ]]; then
-    $sudo $dockerapp logs -f $2
-  else
-    $sudo $dockerapp logs --since=$3 $2
-  fi
-  ;;
-attach | at)
-  echo "Ctrl+p, Ctrl+q pour quitter"
-  $sudo $dockerapp attach $2
-  ;;
-stats | ps)
-  $sudo $dockerapp stats --format "table {{.Name}}\t {{.CPUPerc}}\t {{.MemUsage}}"
-  ;;
-statsall | psa)
-  $sudo $dockerapp stats --format "table {{.Name}}\t {{.CPUPerc}}\t {{.MemPerc}}\t {{.MemUsage}}\t {{.NetIO}}\t {{.BlockIO}}"
-  ;;
-bash | sh)
-  $sudo $dockerapp exec -it $2 sh
-  ;;
-networks | n)
-  $sudo $dockerapp network ls
-  ;;
-volumes | v)
-  $sudo $dockerapp volume ls
-  ;;
-backup | bk)
-  if [[ ! -z "$2" ]]; then
-    if [[ -d $containersdir/$2 ]]; then
-      if [[ ! -d $destbackup/$2/.old ]]; then
-        sudo mkdir -p $destbackup/$2/.old
-        sudo chown -R $user: $destbackup
+      if [[ ! -f $dir/cfg/$app/compose.yml ]] || [[ -z "$1" ]]; then
+        echo "Application $app non trouvée"
+        echo ""
+      else
+        $sudo $compose -f $dir/cfg/$app/compose.yml up -d
       fi
-      $dir/jdocker.sh rm $2
-      cd $containersdir
-      num=0
-      tarlist=.$2.$num.list
-      tarname=$2.$num.tar.gz
-      case $3 in
-      f | full)
-        if [[ -f .$2.0.list ]]; then
-          sudo rm -f .$2.*.list
-        fi
-        if [[ -f $destbackup/$2/$2.0.tar.gz ]]; then
-          sudo mv $destbackup/$2/$2.*.gz $destbackup/$2/.old
-        fi
-        echo "Sauvegarde full de $2..."
-        sudo tar czg $tarlist -f $tarname $2
-        sudo chown $user: $tarname
-        sudo mv $tarname $destbackup/$2
-        ;;
-      i | incr)
-        if [[ ! -f .$2.0.list ]]; then
-          echo "Sauvegarde full introuvable. Arrêt"
-          exit 0
-        fi
-        while [ -f $tarlist ]; do
-          num=$(($num + 1))
-          tarlist=.$2.$num.list
-          tarname=$2.$num.tar.gz
-        done
-        sudo cp .$2.$(($num - 1)).list $tarlist
-        echo "Sauvegarde incrémentielle de $2..."
-        sudo tar czg $tarlist -f $tarname $2
-        sudo chown $user: $tarname
-        sudo mv $tarname $destbackup/$2
-        ;;
-      *)
-        echo "Sauvegarde de $2..."
-        sudo tar czf $2.$(date '+%Y%m%d').tar.gz $2
-        sudo chown $user: $2.$(date '+%Y%m%d').tar.gz
-        sudo mv $2.$(date '+%Y%m%d').tar.gz $destbackup/$2
-        sudo find $destbackup/$2 -name $2.*.gz -mtime +7 -exec rm {} \;
-        ;;
-      esac
-      echo "Sauvegarde terminée. Relance..."
-      $dir/jdocker.sh it $2
-    else
-      echo "Dossier $containersdir/$2 non trouvé"
+    done
+    ;;
+  remove | rm)
+    shift
+    for app in $*; do
+      if [[ ! -f $dir/cfg/$app/compose.yml ]] || [[ -z "$1" ]]; then
+        echo "Application $app non trouvée"
+        echo ""
+      else
+        $sudo $compose -f $dir/cfg/$app/compose.yml down
+      fi
+    done
+    ;;
+  restart | r)
+    if [[ ! -z "$2" ]]; then
+      shift
+      for app in $*; do
+        $sudo $dockerapp restart $app
+      done
     fi
-  else
-    if [[ -f $dir/.jdocker.cron ]]; then
-      sudo cp -v $dir/.jdocker.cron /etc/cron.d/jdocker
-      sudo sed -i "s,DIR,$dir," /etc/cron.d/jdocker
-    else
-      echo "Fichier $dir/.jdocker.cron absent"
+    ;;
+  purge | pr)
+    $sudo $dockerapp system prune -f
+    ;;
+  purgeall | pra)
+    $sudo $dockerapp system prune -f -a --volumes
+    ;;
+  load | lo)
+    if [[ ! -d $imgdir/.old ]]; then
+      mkdir -p $imgdir/.old
     fi
-  fi
-  ;;
-* | help)
-  cat $dir/.jdocker.help
-  ;;
+    for file in $(ls $imgdir/*.tar); do
+      $sudo $dockerapp load -i $file
+      mv $file $imgdir/.old
+    done
+    ;;
+  upgrade | up)
+    if [[ ! -z "$2" ]]; then
+      shift
+      for app in $*; do
+        $dir/jdocker.sh rm $app
+        $dir/jdocker.sh it $app
+      done
+    else
+      $sudo $dockerapp images | grep -v ^REPO | sed 's/ \+/:/g' | cut -d: -f1,2 | xargs -L1 $sudo $dockerapp pull
+    fi
+    ;;
+  logs | l)
+    if [[ -z "$3" ]]; then
+      $sudo $dockerapp logs -f $2
+    else
+      $sudo $dockerapp logs --since=$3 $2
+    fi
+    ;;
+  attach | at)
+    echo "Ctrl+p, Ctrl+q pour quitter"
+    $sudo $dockerapp attach $2
+    ;;
+  stats | ps)
+    $sudo $dockerapp stats --format "table {{.Name}}\t {{.CPUPerc}}\t {{.MemUsage}}"
+    ;;
+  statsall | psa)
+    $sudo $dockerapp stats --format "table {{.Name}}\t {{.CPUPerc}}\t {{.MemPerc}}\t {{.MemUsage}}\t {{.NetIO}}\t {{.BlockIO}}"
+    ;;
+  bash | sh)
+    $sudo $dockerapp exec -it $2 sh
+    ;;
+  networks | n)
+    $sudo $dockerapp network ls
+    ;;
+  volumes | v)
+    $sudo $dockerapp volume ls
+    ;;
+  backup | bk)
+    if [[ ! -z "$2" ]]; then
+      if [[ -d $containersdir/$2 ]]; then
+        if [[ ! -d $destbackup/$2/.old ]]; then
+          sudo mkdir -p $destbackup/$2/.old
+          sudo chown -R $user: $destbackup
+        fi
+        $dir/jdocker.sh rm $2
+        cd $containersdir
+        num=0
+        tarlist=.$2.$num.list
+        tarname=$2.$num.tar.gz
+        case $3 in
+          f | full)
+            if [[ -f .$2.0.list ]]; then
+              sudo rm -f .$2.*.list
+            fi
+            if [[ -f $destbackup/$2/$2.0.tar.gz ]]; then
+              sudo mv $destbackup/$2/$2.*.gz $destbackup/$2/.old
+            fi
+            echo "Sauvegarde full de $2..."
+            sudo tar czg $tarlist -f $tarname $2
+            sudo chown $user: $tarname
+            sudo mv $tarname $destbackup/$2
+            ;;
+          i | incr)
+            if [[ ! -f .$2.0.list ]]; then
+              echo "Sauvegarde full introuvable. Arrêt"
+              exit 0
+            fi
+            while [ -f $tarlist ]; do
+              num=$(($num + 1))
+              tarlist=.$2.$num.list
+              tarname=$2.$num.tar.gz
+            done
+            sudo cp .$2.$(($num - 1)).list $tarlist
+            echo "Sauvegarde incrémentielle de $2..."
+            sudo tar czg $tarlist -f $tarname $2
+            sudo chown $user: $tarname
+            sudo mv $tarname $destbackup/$2
+            ;;
+          *)
+            echo "Sauvegarde de $2..."
+            sudo tar czf $2.$(date '+%Y%m%d').tar.gz $2
+            sudo chown $user: $2.$(date '+%Y%m%d').tar.gz
+            sudo mv $2.$(date '+%Y%m%d').tar.gz $destbackup/$2
+            sudo find $destbackup/$2 -name $2.*.gz -mtime +7 -exec rm {} \;
+            ;;
+        esac
+        echo "Sauvegarde terminée. Relance..."
+        $dir/jdocker.sh it $2
+      else
+        echo "Dossier $containersdir/$2 non trouvé"
+      fi
+    else
+      if [[ -f $dir/.jdocker.cron ]]; then
+        sudo cp -v $dir/.jdocker.cron /etc/cron.d/jdocker
+        sudo sed -i "s,DIR,$dir," /etc/cron.d/jdocker
+      else
+        echo "Fichier $dir/.jdocker.cron absent"
+      fi
+    fi
+    ;;
+  * | help)
+    cat $dir/.jdocker.help
+    ;;
 esac
