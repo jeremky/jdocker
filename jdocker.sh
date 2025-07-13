80b918fe20dad342f1a4128dc395c45223ac4959
JeremKy <jeremky@me.com>
2025-07-13 22:03:03 +0200

    Mise à jour

--- jdocker.sh
+++ jdocker.sh
@@ -240,6 +240,7 @@ case $1 in
     ;;
   lzd | lazydocker)
     if [[ ! -f ~/.local/bin/lazydocker ]]; then
+      cd $HOME
       curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
     fi
     export DOCKER_HOST=unix:///var/run/user/$(id -g)/podman/podman.sock
