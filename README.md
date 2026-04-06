35e47658f0f8ecf0c363a836b2741e3d319e8744
JeremKy <jeremky@me.com>
2026-04-06 14:26:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# jdocker.sh
+
+Ce script permet une administration plus simplifiée des conteneurs Podman en mode rootless.
+Les fichiers de déploiement sont centralisés dans le répertoire de votre choix, ce qui permet de les déployer facilement, sans avoir besoin d'être dans le dossier où se trouve le fichier `compose.yml`.
+
+## Configuration
+
+Avant d'utiliser ce script, vous devez tout d'abord renommer le fichier `.jdocker.cfg.template` en `jdocker.cfg`, pour ensuite y indiquer les informations suivantes :
+
+- Votre nom d'utilisateur, utilisé pour la création des dossiers nécessaires au bon fonctionnement du script
+
+- L'application de composition à utiliser. Si vous n'en avez pas déjà, laissez `podman-compose`. Il sera automatiquement installé lors du premier lancement du script
+
+- Le port minimal autorisé utilisé par vos conteneurs (80 par défaut)
+
+- Les différents répertoires où sont stockées les données (les fichiers `compose.yml`, les backups, les volumes...)
+
+```txt
+# jdocker config
+user=votre_user
+
+# applications compose à utiliser (remplacez par docker-compose si podman-docker est installé)
+compose=podman-compose
+
+# port minimal à autoriser
+port=80
+
+# dossier des fichiers compose
+configdir=$dir/cfg
+
+# dossier des volumes
+containersdir=/opt/containers
+
+# dossier des sauvegardes
+destbackup=/home/$user/backups
+
+# rétention des sauvegardes (en jours)
+retention=7
+
+# dossier des images locales
+imgdir=/tmp/dockerimg
+
+# sauvegarde automatique lors de l'upgrade
+autobackup=false
+
+# ménage automatique après upgrade
+autoclean=true
+
+```
+
+> le dossier `configdir` doit contenir un sous dossier pour chaque application, avec un fichier `compose.yml` et un fichier `.env`
+
+## Utilisation
+
+Lancez le script une première fois pour installer Podman et l'auto complétion.
+Si vous déplacez le script, supprimez le fichier `/etc/bash_completion.d/jdocker` et relancez-le.
+
+Pour consulter l'aide, lancez `./jdocker.sh` sans paramètre :
+
+```txt
+Commandes disponibles :
+  ls  | list            Lister les conteneurs actifs
+  lsa | listall         Lister les conteneurs actifs avec les ports et l'image utilisée
+  n   | networks        Lister les réseaux virtuels
+  v   | volumes         Lister les volumes virtuels
+  i   | images          Lister les images
+  l   | logs            Consulter les logs pour un conteneur spécifié
+  lo  | load            Charger une ou plusieurs images locales spécifiées
+  it  | install         Installer un conteneur avec compose
+  rm  | remove          Supprimer un conteneur avec compose
+  r   | restart         Redémarrer un conteneur
+  pr  | purge           Purger les images et les réseaux non utilisés
+  pra | purgeall        Purger également les volumes non utilisés
+  at  | attach          S'attacher au prompt ouvert pour un conteneur spécifié
+  p   | pull            Récupérer la dernière version de l'image d'un conteneur spécifié
+  up  | upgrade         Télécharger la dernière image et mettre à jour un conteneur spécifié
+  ps  | stats           Afficher les statistiques des conteneurs en cours d'exécution
+  psa | statsall        Afficher les statistiques des conteneurs détaillées
+  sh  | bash            Se connecter au bash d'un conteneur spécifié
+  bk  | backup          Sauvegarder un conteneur spécifié
+  u   | unshare         Basculer l'ID via la commande podman unshare
+  lzd | lazydocker      Lance l'outil lazydocker et l'installe s'il n'est pas présent
+  h   | help            Afficher cette aide
+```
+
+## Sauvegarde
+
+`jdocker.sh` propose un système de sauvegarde des volumes.
+Pour automatiser vos sauvegardes, renommez le fichier `.jdocker.cron.template` en `jdocker.cron`, puis adaptez-le selon vos préférences.
+Exécutez ensuite `./jdocker.sh bk` pour le copier automatiquement dans le répertoire `/etc/cron.d`.
+
+```txt
+# jdocker cron
+jdocksh=$dir/jdocker.sh
+
+# exemple1
+0 0 * * *  $user $jdocksh bk exemple1 >/dev/null 2>&1
+
+# exemple2
+0 1 * * *  $user $jdocksh bk exemple2 >/dev/null 2>&1
+```
---
+++ jdocker.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+
+dir=$(dirname "$(realpath "$0")")
+
+# Messages en couleur
+error()   { echo -e "\033[0;31m====> $*\033[0m" ;}
+message() { echo -e "\033[0;32m====> $*\033[0m" ;}
+warning() { echo ; echo -e "\033[0;33m====> $*\033[0m" ;}
+
+# Chargement du fichier de config
+cfg="$dir/jdocker.cfg"
+if [[ -f $cfg ]]; then
+  . $cfg
+else
+  error "Fichier $cfg introuvable"
+  exit 1
+fi
+
+# Installation de Podman
+if [[ ! -f /usr/bin/podman && -f /usr/bin/apt ]]; then
+  warning "Installation de Podman..."
+  sudo apt install -y podman podman-compose
+  sudo mkdir -p $containersdir
+  sudo chown $user: $containersdir
+  sudo sysctl net.ipv4.ip_unprivileged_port_start=$port
+  echo "net.ipv4.ip_unprivileged_port_start=$port" | sudo tee /etc/sysctl.d/10-podman.conf
+  sudo loginctl enable-linger $user
+  systemctl enable --user --now podman-restart.service podman.socket
+  message "Installation de Podman terminée"
+fi
+
+# Installation de la complétion
+if [[ ! -f /etc/bash_completion.d/jdocker ]]; then
+  sudo cp $dir/.jdocker.comp /etc/bash_completion.d/jdocker
+  export containersdir configdir imgdir
+  envsubst '$configdir $containersdir $imgdir' < "$dir/.jdocker.comp" | sudo tee /etc/bash_completion.d/jdocker > /dev/null
+  echo
+  message "Auto complétion installée. Redémarrez la session ou chargez la complétion avec :"
+  echo "  source /etc/bash_completion"
+  echo
+  exit 0
+fi
+
+checkarg() {
+  if [[ $# -eq 0 ]]; then
+    error "Aucune application spécifiée en paramètre"
+    return 1
+  fi
+}
+
+process() {
+  local action=$1
+  shift
+  for app in $@; do
+    if [[ ! -f $configdir/$app/compose.yml ]]; then
+      echo
+      error "Fichier compose.yml pour $app introuvable, $action impossible"
+      continue
+    fi
+    case $action in
+      install)
+        if ! podman container exists $app; then
+          warning "Déploiement de $app..."
+          $compose -f $configdir/$app/compose.yml up -d
+          message "Application $app déployée"
+        else
+          echo
+          error "Application $app déjà déployée"
+        fi
+        ;;
+      remove)
+        if podman container exists $app; then
+          warning "Suppression de $app..."
+          $compose -f $configdir/$app/compose.yml down
+          message "Application $app supprimée"
+        else
+          echo
+          error "Application $app non déployée"
+        fi
+        ;;
+      pull)
+        if ! grep -q "image:.*localhost" $configdir/$app/compose.yml; then
+          warning "Récupération de la nouvelle image de $app..."
+          podman pull $(grep "image:" $configdir/$app/compose.yml | awk '{print $2}')
+          message "Nouvelle image $app récupérée"
+        fi
+        ;;
+      backup)
+        if [[ -d $containersdir/$app ]]; then
+          if podman container exists $app; then
+            restartafter=1
+            process remove $app
+          fi
+          mkdir -p $destbackup/$app
+          warning "Sauvegarde de $app..."
+          bckfile=$destbackup/$app/$app.$(date '+%Y%m%d%H%M').tar.gz
+          podman unshare bash -c "tar -C $containersdir -czf $bckfile $app && chown root: $bckfile"
+          find $destbackup/$app -name $app.*.gz -mtime +$retention -exec rm {} \;
+          ls $bckfile
+          message "Sauvegarde terminée"
+          if (($restartafter)); then
+            process install $app
+          fi
+        fi
+        ;;
+    esac
+  done
+}
+
+purge() {
+  options=$@
+  warning "Suppression des données non utilisées..."
+  podman system prune $options
+  message "Nettoyage terminé"
+}
+
+# Commandes
+case $1 in
+  ls | list)
+    podman container ls -a --format "table {{.Names}} \t {{.Status}}"
+    ;;
+  lsa | listall)
+    podman container ls -a --format "table {{.Names}} \t {{.Status}} \t {{.Ports}} \t {{.Image}}"
+    ;;
+  it | install)
+    shift
+    checkarg $@ || exit 1
+    process install $@
+    echo
+    ;;
+  rm | remove)
+    shift
+    checkarg $@ || exit 1
+    process remove $@
+    echo
+    ;;
+  r | restart)
+    shift
+    checkarg $@ || exit 1
+    for app in $@; do
+      podman restart $app
+    done
+    ;;
+  pr | purge)
+    purge -a -f
+    echo
+    ;;
+  pra | purgeall)
+    purge -a --volumes
+    echo
+    ;;
+  lo | load)
+    shift
+    for img in $@; do
+      if [[ ! -f $imgdir/$img ]]; then
+        error "Fichier $img non trouvé dans $imgdir"
+      else
+        podman load -i $imgdir/$img
+      fi
+    done
+    ;;
+  up | upgrade)
+    shift
+    checkarg $@ || exit 1
+    for app in $@; do
+      process pull $app
+      process remove $app
+      [[ $autobackup = true ]] && process backup $app
+      process install $app
+    done
+    [[ $autoclean = true ]] && purge -a -f
+    echo
+    ;;
+  p | pull)
+    if [[ -n "$2" ]]; then
+      shift
+      process pull $@
+      echo
+    else
+      podman images --format "{{.Repository}}:{{.Tag}}" | grep -v '^localhost' | xargs -r -L1 podman pull
+    fi
+    ;;
+  l | logs)
+    if [[ -z "$3" ]]; then
+      podman logs -f $2
+    else
+      podman logs --since=$3 $2
+    fi
+    ;;
+  at | attach)
+    warning "Ctrl+p, Ctrl+q pour quitter"
+    podman attach $2
+    ;;
+  ps | stats)
+    podman stats --format "table {{.Name}}\t {{.CPUPerc}}\t {{.MemUsage}}"
+    ;;
+  psa | statsall)
+    podman stats --format "table {{.Name}}\t {{.CPUPerc}}\t {{.MemPerc}}\t {{.MemUsage}}\t {{.NetIO}}\t {{.BlockIO}}"
+    ;;
+  sh | bash)
+    podman exec -it $2 sh
+    ;;
+  n | networks)
+    podman network ls
+    ;;
+  i | images)
+    podman images
+    ;;
+  u | unshare)
+    shift
+    if [ $# -eq 0 ]; then
+      podman unshare bash --rcfile <(echo '
+      source ~/.bashrc
+      PS1="\[\033[01;33m\]unshare@\h\[\033[00m\]:\[\033[01;34m\]\w \$\[\033[00m\] "
+      ')
+    else
+      podman unshare --rootless-netns $@
+    fi
+    ;;
+  v | volumes)
+    podman volume ls
+    ;;
+  bk | backup)
+    if [[ -n "$2" ]]; then
+      shift
+      process backup $@
+      echo
+    else
+      if [[ -f $dir/jdocker.cron ]]; then
+        export dir user
+        envsubst '$dir $user' < "$dir/jdocker.cron" | sudo tee /etc/cron.d/jdocker > /dev/null
+        echo
+        message "Fichier /etc/cron.d/jdocker en place"
+        cat /etc/cron.d/jdocker
+        echo
+      else
+        error "Fichier $dir/jdocker.cron absent"
+      fi
+    fi
+    ;;
+  lzd | lazydocker)
+    if [[ ! -f ~/.local/bin/lazydocker ]]; then
+      ( cd $HOME && curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash )
+    fi
+    export DOCKER_HOST=unix:///var/run/user/$(id -g)/podman/podman.sock
+    ~/.local/bin/lazydocker
+    ;;
+  * | help)
+    echo
+    message "Commandes disponibles :"
+    cat $dir/.jdocker.help
+    echo
+    ;;
+esac
